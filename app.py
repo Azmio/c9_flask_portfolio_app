ffc3d13b231d9f78a0d199f3506872f67efecbed
Azmio <34865761+Azmio@users.noreply.github.com>
2017-12-26 18:47:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	modiname= name+ 'The wise one' 
+	modiname= name+ ' The wise one' 
 	return render_template('index.html', name=modiname)
 
 
